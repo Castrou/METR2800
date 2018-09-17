1f154d1ffe4e996e691c448ccf1799c4c33c2393
Bryan Greenham <bryan.greenham@gmail.com>
2018-09-17 17:45:26 +1000

    Deleted old files
    Delete detect.c
    Deleted detect.h
    deleted main.c
    
    (all victor files)

--- detect.h
+++
@@ -1,7 +0,0 @@
-#define PHOTODIODE1 0b101 //PC5
-#define PHOTODIODE2 0b100  //PC4
-#define PHOTOTRANSISTOR1 0b011 //PC3
-#define PHOTOTRANSISTOR2 0b010 //PC2
-int sensors[4];
-int adcvalues[4];
-void detect (void);
